a2ee0f848f5f749ccf74dd1ed65a34f6b1c92b07
Amir Moualem <amoualem@gmail.com>
2018-07-26 22:17:29 +0300

    initial skeleton multi-output support

--- py/cli/main.py
+++ py/cli/main.py
@@ -1,18 +1,25 @@
 import argparse
 
+from outputs import slack
 from outputs import stdout
 from sourcecontrol import github
 
 
-def main(owner, repository, accessToken=None):
+def main(owner, repository, outputMethod, accessToken=None):
     pullrequestInfo = github.listPullRequests(owner, repository, accessToken)
-    stdout.send(pullrequestInfo)
 
+    if outputMethod == 'stdout':
+        stdout.send(pullrequestInfo)
+    elif outputMethod == 'slack':
+        slack.send(pullrequestInfo)
+    else:
+        raise Exception("invalid output method %s" % outputMethod)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get pull request status")
     parser.add_argument("owner", help="the name of the repository's owner")
     parser.add_argument("repository", help="the name of the repository to get PR status for")
     parser.add_argument("--accessToken", help="a personal access token to use for privileged repositories")
+    parser.add_argument("--outputMethod", help="output method. may be stdout (default) or slack (requires OAuth token)", default="stdout")
     args = parser.parse_args()
-    main(args.owner, args.repository, args.accessToken)
+    main(args.owner, args.repository, args.outputMethod, args.accessToken)
