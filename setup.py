a927f4c17449200e18f496ba5e1e5098fbd8f9f7
Amir Moualem <amoualem@gmail.com>
2018-07-29 19:49:02 +0300

    refactors to simplify distribution

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bleachrepo",
-    version="0.0.1",
+    version="0.0.7",
     author="Amir Moualem",
     author_email="amoualem@gmail.com",
     description="Find and remove the mold from your source control repositories",
