5864096a84631f5e24146cd9b745cfda74ff6027
Amir Moualem <amoualem@gmail.com>
2018-07-31 09:12:57 +0300

    basic slack support

--- bleach/outputs/slack.py
+++ bleach/outputs/slack.py
@@ -1,2 +1,15 @@
+import os
+import slackclient
+
+
 def send(data):
-    pass
+    assert 'BLEACH_SLACK_CHANNEL' in os.environ
+    assert 'BLEACH_SLACK_ACCESS_TOKEN' in os.environ
+
+    slackClient = slackclient.SlackClient(os.environ['BLEACH_SLACK_ACCESS_TOKEN'])
+
+    slackClient.api_call(
+        "chat.postMessage",
+        channel=os.environ['BLEACH_SLACK_CHANNEL'],
+        text=data,
+    )
