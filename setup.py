965c9b2c8a9bf35efb88a9fc75aa405a144c0daa
Amir Moualem <amoualem@gmail.com>
2018-08-02 19:53:00 +0300

    adding required packages to setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bleachrepo",
-    version="0.0.8",
+    version="0.0.12",
     author="Amir Moualem",
     author_email="amoualem@gmail.com",
     description="Find and remove the mold from your source control repositories",
@@ -18,4 +18,10 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
+    install_requires=[
+        "slackclient",
+        "requests",
+        "python-dateutil",
+        "pytz",
+    ],
 )
