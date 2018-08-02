1ec380bdd10a040a2f8b32e2e26a2b0142b32fc9
Amir Moualem <amoualem@gmail.com>
2018-08-02 19:10:43 +0300

    basic output formatter for sorting and filtering

--- bleach/formatters/default.py
+++ bleach/formatters/default.py
@@ -1,2 +1,33 @@
-def doFormat(output):
-    return output
+LINE_TEMPLATE = "{title}\nopen for {days} days - owner is {user}"
+
+
+def doFormat(data):
+    return _listOfOldestRequests(data)
+
+
+def _listOfOldestRequests(data, countOfOldest=5):
+    assert type(data) == list
+    sortedByDate = sorted(data, key=lambda record: record["createdAt"])
+    recordsToKeep = sortedByDate[:countOfOldest]
+
+    formattedData = []
+    for record in recordsToKeep:
+        daysAlive = _calculateDaysAlive(record["createdAt"])
+        formattedLine = LINE_TEMPLATE.format(
+            title=record["title"],
+            days=daysAlive,
+            user=record["user"]
+        )
+        formattedData.append(formattedLine)
+
+    return "\n\n".join(formattedData)
+
+
+def _calculateDaysAlive(stringOfDateFrom):
+    import datetime
+    from dateutil import parser
+    import pytz
+    now = datetime.datetime.utcnow().replace(tzinfo=pytz.utc)
+    dateFrom = parser.parse(stringOfDateFrom)
+
+    return (now - dateFrom).days
