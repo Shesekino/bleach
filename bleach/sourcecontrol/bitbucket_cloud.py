4f1313d6f0fd63e7ce5cb9413b214ff0f0b02163
Amir Moualem <amoualem@gmail.com>
2018-08-05 19:59:39 +0300

    adding a skeleton for bitbucket-cloud

---
+++ bleach/sourcecontrol/bitbucket_cloud.py
@@ -0,0 +1,12 @@
+import requests
+
+from bleach import config
+from bleach.models import pullrequest
+
+
+def listPullRequests():
+    raise NotImplementedError("TODO")
+
+
+def _getUrl():
+    raise NotImplementedError("TODO")
