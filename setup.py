31854c155588394d58391b6b555af8fb0e353d44
Amir Moualem <amoualem@gmail.com>
2018-08-06 20:47:04 +0300

    bumping version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="bleachrepo",
-    version="0.0.14",
+    version="0.0.15",
     author="Amir Moualem",
     author_email="amoualem@gmail.com",
     description="Find and remove the mold from your source control repositories",
