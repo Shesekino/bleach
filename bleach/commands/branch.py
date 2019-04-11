d025ba73470bd24a3d1b54fa30288381c907cdb3
Anton Drukh <anton@snyk.io>
2019-04-11 10:06:55 +0300

    fix: be silent when primary and secondary branches are in sync
    The idea is to ditch non-actionable notifications :)

--- bleach/commands/branch.py
+++ bleach/commands/branch.py
@@ -7,17 +7,18 @@ from bleach.sourcecontrol import github
 def main(owner, repository, primary, secondary):
     # who needs to check the optional parameters?
     missingCommits = github.missingCommits(owner, repository, primary, secondary)
+    hasSomethingToSay = False
+    badlyFormattedOutput = ""
     # TODO format the output
-    TEMPLATE_MISS = "*{repo}*\nprimary branch {primary} seems to be missing commits from secondary branch {secondary}"
-    TEMPLATE_OK = "*{repo}*\nprimary branch {primary} seems to be synced with secondary branch {secondary}"
+    TEMPLATE_MISS = "*{repo}*\nprimary branch `{primary}` seems to be missing commits from secondary branch `{secondary}`"
     if missingCommits:
+        hasSomethingToSay = True
         badlyFormattedOutput = TEMPLATE_MISS.format(repo=repository, primary=primary, secondary=secondary)
-    else:
-        badlyFormattedOutput = TEMPLATE_OK.format(repo=repository, primary=primary, secondary=secondary)
     outputMethod = config.CONFIG["outputMethod"]
-    if outputMethod == 'stdout':
-        stdout.send(badlyFormattedOutput)
-    elif outputMethod == 'slack':
-        slack.send(badlyFormattedOutput)
-    else:
-        raise Exception("invalid output method %s" % outputMethod)
+    if hasSomethingToSay:
+        if outputMethod == 'stdout':
+            stdout.send(badlyFormattedOutput)
+        elif outputMethod == 'slack':
+            slack.send(badlyFormattedOutput)
+        else:
+            raise Exception("invalid output method %s" % outputMethod)
