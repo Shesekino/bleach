fb26e1fdb9d911b4eb5d00e074cc9c9522698723
Amir Moualem <amoualem@gmail.com>
2019-04-22 23:47:34 +0300

    fix: branch output for non-discrepancy

--- bleach/commands/branch.py
+++ bleach/commands/branch.py
@@ -11,9 +11,12 @@ def main(owner, repository, primary, secondary):
     badlyFormattedOutput = ""
     # TODO format the output
     TEMPLATE_MISS = "*{repo}*\nprimary branch `{primary}` seems to be missing commits from secondary branch `{secondary}`"
+    TEMPLATE_OK = "*{repo}*\nprimary branch {primary} seems to be synced with secondary branch {secondary}"
     if missingCommits:
         hasSomethingToSay = True
         badlyFormattedOutput = TEMPLATE_MISS.format(repo=repository, primary=primary, secondary=secondary)
+    else:
+        badlyFormattedOutput = TEMPLATE_OK.format(repo=repository, primary=primary, secondary=secondary)
     outputMethod = config.CONFIG["outputMethod"]
     if hasSomethingToSay or not config.CONFIG['printOnlyActionable']:
         if outputMethod == 'stdout':
